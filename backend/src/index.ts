9edcbed9c57b0a5dd778d3ca178542d2a9a926f4
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2026-02-27 02:18:15 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,10 @@ import { errorHandler } from './middlewares/error.middleware';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://task-dashboard-nu-one.vercel.app'],
+    credentials: true,
+}));
 app.use(express.json());
 
 // Routes
